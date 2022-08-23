ca4e8bc8091ba30f9983fbfdb2a16ba22a6bfe3a
Rick Hull <rick.hull@gmail.com>
2022-08-23 16:34:56 -0400

    polish

--- demo/celsius.rb
+++ demo/celsius.rb
@@ -2,8 +2,10 @@ require 'backprop'
 
 include BackProp
 
+deg_f = 68.5
+
+puts "Convert #{deg_f} to celsius using arithmetic (F - 32 * 5/9):"
+
 f = v(68.5, label: :f)
 c = (f - 32) * 5 / 9; c.label = :c
-c.backward
-
 p c
