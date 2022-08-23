078106e9ea6d3523425abc69ab41d7099a99ef89
Rick Hull <rick.hull@gmail.com>
2022-08-23 17:57:22 -0400

    add comments and use MLP#descend

--- demo/loss.rb
+++ demo/loss.rb
@@ -23,13 +23,22 @@ puts "Press Enter to continue"
 gets
 
 999.times { |i|
+  # 1. apply inputs to the net to yield predictions
+  # 2. calculate the loss
+  # 3. backward propagate the gradients
+  # 4. adjust every neuron in the direction of minimizing loss
+
+  # 1. apply inputs
   predictions = inputs.map { |input| n.apply(input) }
+
+  # 2. calculate loss
   loss = BackProp.mean_squared_error(outputs, predictions)
   puts loss
 
-  # propagate the derivatives (gradients) backwards
+  # 3. propagate the derivatives (gradients) backwards
   loss.backward
 
+  # output every so often
   if i % 100 == 0
     p predictions.map(&:value)
     p n
@@ -37,10 +46,8 @@ gets
     gets
   end
 
-  # nudge all parameters according to the new gradient
-  n.parameters.each { |val|
-    val.value += -1 * gradient_step * val.gradient
-  }
+  # 4. adjust all weights and biases towards minimizing loss function
+  n.descend(gradient_step)
 }
 
 p predictions.map(&:value)
