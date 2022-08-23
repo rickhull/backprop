8ecf3d645b01f7a61a961ebba5d3cedf6a3cbac6
Rick Hull <rick.hull@gmail.com>
2022-08-23 16:21:06 -0400

    perform gradient descent

--- demo/loss.rb
+++ demo/loss.rb
@@ -5,18 +5,43 @@ include BackProp
 num_inputs = 3
 num_examples = 9
 net_structure = [4, 4, 1]
+gradient_step = 0.1
+iterations = 999
 
 # binary classifier; 9 sets of inputs that map to 1 or 0
 inputs = BackProp.rand_inputs(num_inputs, num_examples, (-1.0..1.0))
 outputs = BackProp.rand_outputs(num_examples, 2)
+predictions = []
 
 n = MLP.new(num_inputs, net_structure)
+inputs.each.with_index { |input, i|
+  puts format("%s = %s", input.join(', '), outputs[i].value.inspect)
+}
 puts n
 
-preds = inputs.map { |input| n.apply(input) }
-loss = BackProp.mean_squared_error(outputs, preds)
-puts loss
+puts "Press Enter to continue"
+gets
 
+999.times { |i|
+  predictions = inputs.map { |input| n.apply(input) }
+  loss = BackProp.mean_squared_error(outputs, predictions)
+  puts loss
 
-loss.backward
+  # propagate the derivatives (gradients) backwards
+  loss.backward
+
+  if i % 100 == 0
+    p predictions.map(&:value)
+    p n
+    puts
+    gets
+  end
+
+  # nudge all parameters according to the new gradient
+  n.parameters.each { |val|
+    val.value += -1 * gradient_step * val.gradient
+  }
+}
+
+p predictions.map(&:value)
 p n
